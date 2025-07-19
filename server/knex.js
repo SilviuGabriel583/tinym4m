c5cf9c78630bda9429bc49deef897a946b2718fb
SilviuGabriel583 <droidcam734@gmail.com>
2025-07-19 05:34:35 +0300

    Update knex.js

--- server/knex.js
+++ server/knex.js
@@ -15,7 +15,9 @@ const db = knex({
     database: env.DB_NAME,
     user: env.DB_USER,
     password: env.DB_PASSWORD,
-    ssl: env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    ssl: {
+      rejectUnauthorized: false
+    },
     pool: {
       min: env.DB_POOL_MIN,
       max: env.DB_POOL_MAX
